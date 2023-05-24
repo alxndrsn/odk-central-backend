3c080885f7e245bf72da62b68c215a46304249f8
alxndrsn <alxndrsn>
2023-05-24 12:21:12 +0000

    Add playwright config

---
+++ oidc-tester/playwright.config.js
@@ -0,0 +1,109 @@
+
+const { devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+  testDir: './',
+  /* Maximum time one test can run for. */
+  timeout: 10 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 2000
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'line',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://odk-central.example.org:8989',
+
+    /* Collect trace when test fails. See https://playwright.dev/docs/trace-viewer */
+    trace: 'retain-on-failure',
+
+    screenshot: 'only-on-failure',
+
+    // desperate debug options
+    video: 'retain-on-failure',
+    headless: true,
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    //{
+    //  name: 'chromium',
+    //  use: {
+    //    ...devices['Desktop Chrome'],
+    //    viewport: { width: 1280, height: 2800 }, // make sure we can see everything in failure screenshots; maybe not a good change for long-term
+    //  },
+    //},
+
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: {
+    //     ...devices['Pixel 5'],
+    //   },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: {
+    //     ...devices['iPhone 12'],
+    //   },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: {
+    //     channel: 'msedge',
+    //   },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: {
+    //     channel: 'chrome',
+    //   },
+    // },
+  ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    reuseExistingServer: true,
+    url: 'http://localhost:8383/',
+  },
+};
+
+module.exports = config;
