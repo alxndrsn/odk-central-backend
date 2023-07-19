509ae388e5d66695eab8ee6ac963f2c5cd104a28
alxndrsn <alxndrsn>
2023-07-19 11:10:38 +0000

    Add test: email not verified

---
+++ oidc-tester/playwright-tests/src/authn-success-email-unverified.spec.js
@@ -0,0 +1,24 @@
+// Copyright 2023 ODK Central Developers
+// See the NOTICE file at the top-level directory of this distribution and at
+// https://github.com/getodk/central-backend/blob/master/NOTICE.
+// This file is part of ODK Central. It is subject to the license terms in
+// the LICENSE file found in the top-level directory of this distribution and at
+// https://www.apache.org/licenses/LICENSE-2.0. No part of ODK Central,
+// including this file, may be copied, modified, propagated, or distributed
+// except according to the terms contained in the LICENSE file.
+/* eslint-disable */ // FIXME re-enable lint here
+
+const { test } = require('@playwright/test');
+
+const { frontendUrl } = require('./config');
+const {
+  assertErrorShown,
+  assertLoginSuccessful,
+  fillLoginForm,
+} = require('./utils');
+
+test('handles successful authN, failed authZ', async ({ page }) => {
+  await page.goto(`${frontendUrl}/v1/oidc/login`);
+  await fillLoginForm(page, { username:'charlie', password:'topsecret!!!!!' });
+  await assertErrorShown(page, 'User email not verified');
+});
