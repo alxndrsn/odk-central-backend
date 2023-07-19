5855bba478aaaf0f03b040f33e9702fbdeb6d3b3
alxndrsn <alxndrsn>
2023-07-19 16:15:52 +0000

    config: delete noisy comments

--- oidc-tester/playwright-tests/playwright.config.js
+++ oidc-tester/playwright-tests/playwright.config.js
@@ -38,48 +38,26 @@ const config = {
   testDir: 'src',
   /* Maximum time one test can run for. */
   timeout: 10 * 1000,
-  expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 2000
-  },
-  /* Run tests in files in parallel */
+  expect: { timeout:2000 },
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0, // retries mean failure
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://odk-central.example.org:8989',
-
-    // TODO confirm if required
     ignoreHTTPSErrors: true,
-
-    /* Collect trace when test fails. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-
     screenshot: 'only-on-failure',
-
-    // desperate debug options
+    // desperate debug options - fiddle with these when you're confused what's going on:
     video: 'retain-on-failure',
     headless: true,
   },
-
   projects,
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: 'results/',
-
   globalSetup: require.resolve('./src/global-setup-teardown'),
 };
 
