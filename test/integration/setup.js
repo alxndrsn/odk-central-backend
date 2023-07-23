982ec5a6656f23225698a22b65f53f10cb783cfc
alxndrsn <alxndrsn>
2023-07-23 12:46:03 +0000

    wip

--- test/integration/setup.js
+++ test/integration/setup.js
@@ -130,7 +130,7 @@ const augment = (service) => {
     const users = Array.isArray(userOrUsers) ? userOrUsers : [userOrUsers];
     const tokens = await Promise.all(users.map(async (user) => {
       if(process.env.TEST_AUTH === 'oidc') {
-        const token = await oidcAuthFor(user);
+        const token = await oidcAuthFor(service, user);
         console.log(`TODO: TEST_AUTH=oidc NOT YET FULLY IMPLEMENTED`);
         process.exit(1);
       } else {
@@ -207,26 +207,82 @@ const testTask = (test) => () => new Promise((resolve, reject) => {
   });//.catch(Promise.resolve.bind(Promise));
 });
 
-function oidcAuthFor(user) {
-  const cookieJar = {};
-  function _fetch(url, opts) {
-    // this does not obey all cookie options, e.g. samesite, path etc.
-    // maybe it should.
-    const hostname = 
-  }
+async function oidcAuthFor(service, user) {
+  const makeFetchCookie = require('fetch-cookie');
   try {
     const res1 = await service.get('/v1/oidc/login');
-    console.log(res1.headers);
-    const location = res1.headers.location;
-    const ocvCookie = res1.headers['set-cookie'].find(c => c.startsWith('ocv=')).split(';')[0].replace(/^ocv=/, '')
-    console.log({ location, ocvCookie });
 
-    const res2 = await fetch(location, { method:'GET', redirect:'manual' });
-    console.log(res2);
+    // custom cookie jar probably not important, but we will need these cookies
+    // for the final redirect
+    const cookieJar = new makeFetchCookie.toughCookie.CookieJar();
+    res1.headers['set-cookie'].forEach(cookieString => {
+      cookieJar.setCookie(cookieString, 'http://localhost:8383/v1/oidc/login');
+    });
+    console.log(cookieJar);
+
+    console.log(res1.headers);
+    const location1 = res1.headers.location;
+    console.log({ location1 });
+
+    const fetchC = makeFetchCookie(fetch, cookieJar);
+    const res2 = await fetchC(location1);
+    if(res2.status !== 200) throw new Error('Non-200 response');
+
+    const location2 = await formActionFrom(res2);
+    console.log({ location2 });
+
+    // TODO try replacing with FormData
+    const body = require('querystring').encode({
+      prompt: 'login',
+      login: user,
+      password: 'topSecret123',
+    });
+    console.log(body);
+    const res3 = await fetchC(location2, {
+      method: 'POST', 
+      headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+      body,
+    });
+    console.log('res3:', res3.headers);
+
+    const location3 = await formActionFrom(res3);
+    const body2 = require('querystring').encode({ prompt:'consent' });
+    console.log({ location3 , body2 });
+    const res4 = await fetchC(location3, {
+      method: 'POST', 
+      headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+      body: body2,
+      redirect: 'manual',
+    });
+    console.log('res4:', res4);
+    console.log('res4:', await res4.text());
+    if(res4.status !== 303) throw new Error('Expected 303!');
+
+    console.log(res4.headers);
+    const location4 = res4.headers.get('location');
+    console.log({ location4 });
+    const res5 = await fetchC(location4, { redirect:'manual' });
+    console.log('res5:', res5);
+    console.log('res5:', await res5.text());
+    const location5 = res5.headers.get('location');
+    console.log({ location5 });
+
+    const u5 = new URL(location5);
+    const servicePath = u5.pathname + u5.search;
+    console.log('Requesting from service:', 'GET', servicePath);
+    //const res6 = await service.get(servicePath, { headers:{ cookie:cookieJar.getCookieStringSync(location5) } });
+    const res6 = await service.get(servicePath)
+        .set('Cookie', cookieJar.getCookieStringSync(location5));
+    console.log('res6:', res6);
+
   } catch(err) {
     console.log('OIDC auth failed:', err);
     process.exit(1);
   }
+}
+
+async function formActionFrom(res) {
+  return (await res.text()).match(/<form.*\baction="([^"]*)"/)[1];
 }
 
 module.exports = { testService, testServiceFullTrx, testContainer, testContainerFullTrx, testTask };
