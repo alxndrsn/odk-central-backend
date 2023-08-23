63cd1728076902119de99bacb1bf24f11571e68e
alxndrsn <alxndrsn>
2023-08-23 08:45:08 +0000

    try running tests in parallel on CI

--- oidc-tester/playwright-tests/playwright.config.js
+++ oidc-tester/playwright-tests/playwright.config.js
@@ -42,7 +42,7 @@ const config = {
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 0, // retries mean failure
-  workers: process.env.CI ? 1 : undefined,
+  workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
