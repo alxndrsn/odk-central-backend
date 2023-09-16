efb5778ab26bec413722e6c675f3f99ac2168d42
alxndrsn <alxndrsn>
2023-09-16 05:05:12 +0000

    test/integration: clean up hanging 'threads'
    Resolves need for mocha's `--exit` flag, which the docs say:
    
    > enables a set of potential problems; it’s indicative of tests (or fixtures,
    > harnesses, code under test, etc.) which don’t clean up after themselves
    > properly. Ultimately, "dirty" tests can (but not always) lead to _false
    > positive_ or _false negative_ results.
    
    Flag originally introduced in:
    
    * 472383e14ca6a7994cfd7ae9f563ebefbf74a0c1
    * ca08b535f52a5d712d5c5f5544dcbda9002ec550
    
    Problems resolved:
    
    1. shut down db pool cleanly
    2. clean up yauzl streams (by avoiding them?)

--- test/util/zip.js
+++ test/util/zip.js
@@ -1,9 +1,8 @@
-const tmp = require('tmp');
 const yauzl = require('yauzl');
 const { createWriteStream } = require('fs');
 const streamTest = require('streamtest').v2;
 
-// does all the plumbing work to call the streamer, then unzip and detangle the result.
+// load zipfile into a buffer, then unzip and detangle the result.
 // also, hooraaaayy callback hell.
 // calls the callback with an object as follows:
 // {
@@ -13,54 +12,46 @@ const streamTest = require('streamtest').v2;
 //      …
 // }
 const zipStreamToFiles = (zipStream, callback) => {
-  tmp.file((err, tmpfile) => {
-    // eslint-disable-next-line keyword-spacing
+  zipStream.buffer().parse(binaryParser).end((err, res) => {
     if(err) return callback(err);
 
-    const writeStream = createWriteStream(tmpfile);
-    zipStream.pipe(writeStream);
-    zipStream.on('end', () => {
-      setTimeout(() => {
-        // eslint-disable-next-line no-shadow
-        yauzl.open(tmpfile, { autoClose: false }, (err, zipfile) => {
-          // eslint-disable-next-line keyword-spacing
-          if(err) return callback(err);
+    yauzl.fromBuffer(res.body, (err, zipfile) => {
+      // eslint-disable-next-line keyword-spacing
+      if(err) return callback(err);
 
-          const result = { filenames: [] };
-          const entries = [];
-          let completed = 0;
+      const result = { filenames: [] };
+      const entries = [];
+      let completed = 0;
 
-          zipfile.on('entry', (entry) => entries.push(entry));
-          zipfile.on('end', () => {
-            if (entries.length === 0) {
-              callback(null, result);
-              zipfile.close();
-            } else {
-              entries.forEach((entry) => {
-                result.filenames.push(entry.fileName);
-                // eslint-disable-next-line no-shadow
-                zipfile.openReadStream(entry, (err, resultStream) => {
-                  // eslint-disable-next-line keyword-spacing
-                  if(err) return callback(err);
+      zipfile.on('entry', (entry) => entries.push(entry));
+      zipfile.once('end', err => {
+        if (entries.length === 0) {
+          callback(null, result);
+          zipfile.close();
+        } else {
+          entries.forEach((entry) => {
+            result.filenames.push(entry.fileName);
+            // eslint-disable-next-line no-shadow
+            zipfile.openReadStream(entry, (err, resultStream) => {
 
-                  // eslint-disable-next-line no-shadow
-                  resultStream.pipe(streamTest.toText((err, contents) => {
-                    // eslint-disable-next-line keyword-spacing
-                    if(err) return callback(err);
+              // eslint-disable-next-line keyword-spacing
+              if(err) return callback(err);
 
-                    result[entry.fileName] = contents;
-                    completed += 1;
-                    if (completed === entries.length) {
-                      callback(null, result);
-                      zipfile.close();
-                    }
-                  }));
-                });
-              });
-            }
+              // eslint-disable-next-line no-shadow
+              resultStream.pipe(streamTest.toText((err, contents) => {
+                // eslint-disable-next-line keyword-spacing
+                if(err) return callback(err);
+
+                result[entry.fileName] = contents;
+                completed += 1;
+                if (completed === entries.length) {
+                  callback(null, result);
+                }
+              }));
+            });
           });
-        });
-      }, 5); // otherwise sometimes the file doesn't fully drain
+        }
+      });
     });
   });
 };
@@ -70,3 +61,13 @@ const pZipStreamToFiles = (zipStream) => new Promise((resolve, reject) => { zipS
 
 module.exports = { zipStreamToFiles, pZipStreamToFiles };
 
+function binaryParser(res, callback) {
+  res.setEncoding('binary');
+  res.data = '';
+  res.on('data', function (chunk) {
+    res.data += chunk;
+  });
+  res.on('end', function () {
+    callback(null, new Buffer(res.data, 'binary'));
+  });
+}
