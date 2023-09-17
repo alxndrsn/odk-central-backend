56ab2f71fc57fd475389ffdfb506caeede948dfa
alxndrsn <alxndrsn>
2023-09-17 10:51:50 +0000

    simpler

--- test/util/zip.js
+++ test/util/zip.js
@@ -1,15 +1,6 @@
 const yauzl = require('yauzl');
 const streamTest = require('streamtest').v2;
 
-const binaryParser = (res, callback) => {
-  res.setEncoding('binary');
-  res.data = '';
-  res.on('data', (chunk) => { res.data += chunk; });
-  res.on('end', () => {
-    callback(null, Buffer.from(res.data, 'binary'));
-  });
-};
-
 // load zipfile into a buffer, then unzip and detangle the result.
 // also, hooraaaayy callback hell.
 // calls the callback with an object as follows:
@@ -19,48 +10,44 @@ const binaryParser = (res, callback) => {
 //      {filename}: "contents",
 //      …
 // }
-const zipStreamToFiles = (zipStream, callback) => {
-  zipStream.buffer().parse(binaryParser).end((err, res) => {
+const zipStreamToFiles = (res, callback) => {
+  // eslint-disable-next-line no-shadow
+  yauzl.fromBuffer(res.body, (err, zipfile) => {
     if (err) return callback(err);
 
+    const result = { filenames: [] };
+    const entries = [];
+    let completed = 0;
+
+    zipfile.on('entry', (entry) => entries.push(entry));
     // eslint-disable-next-line no-shadow
-    yauzl.fromBuffer(res.body, (err, zipfile) => {
+    zipfile.on('end', (err) => {
       if (err) return callback(err);
 
-      const result = { filenames: [] };
-      const entries = [];
-      let completed = 0;
+      if (entries.length === 0) {
+        callback(null, result);
+        zipfile.close();
+      } else {
+        entries.forEach((entry) => {
+          result.filenames.push(entry.fileName);
+          // eslint-disable-next-line no-shadow
+          zipfile.openReadStream(entry, (err, resultStream) => {
 
-      zipfile.on('entry', (entry) => entries.push(entry));
-      // eslint-disable-next-line no-shadow
-      zipfile.on('end', (err) => {
-        if (err) return callback(err);
+            if (err) return callback(err);
 
-        if (entries.length === 0) {
-          callback(null, result);
-          zipfile.close();
-        } else {
-          entries.forEach((entry) => {
-            result.filenames.push(entry.fileName);
             // eslint-disable-next-line no-shadow
-            zipfile.openReadStream(entry, (err, resultStream) => {
-
+            resultStream.pipe(streamTest.toText((err, contents) => {
               if (err) return callback(err);
 
-              // eslint-disable-next-line no-shadow
-              resultStream.pipe(streamTest.toText((err, contents) => {
-                if (err) return callback(err);
-
-                result[entry.fileName] = contents;
-                completed += 1;
-                if (completed === entries.length) {
-                  callback(null, result);
-                }
-              }));
-            });
+              result[entry.fileName] = contents;
+              completed += 1;
+              if (completed === entries.length) {
+                callback(null, result);
+              }
+            }));
           });
-        }
-      });
+        });
+      }
     });
   });
 };
