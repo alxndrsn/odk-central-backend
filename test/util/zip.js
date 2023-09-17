961153703d25ec4b3dca3c2776367673fdab48ba
alxndrsn <alxndrsn>
2023-09-17 10:20:06 +0000

    fix lint violations

--- test/util/zip.js
+++ test/util/zip.js
@@ -1,6 +1,14 @@
 const yauzl = require('yauzl');
-const { createWriteStream } = require('fs');
 const streamTest = require('streamtest').v2;
+
+const binaryParser = (res, callback) => {
+  res.setEncoding('binary');
+  res.data = '';
+  res.on('data', (chunk) => { res.data += chunk; });
+  res.on('end', () => {
+    callback(null, Buffer.from(res.data, 'binary'));
+  });
+};
 
 // load zipfile into a buffer, then unzip and detangle the result.
 // also, hooraaaayy callback hell.
@@ -13,18 +21,21 @@ const streamTest = require('streamtest').v2;
 // }
 const zipStreamToFiles = (zipStream, callback) => {
   zipStream.buffer().parse(binaryParser).end((err, res) => {
-    if(err) return callback(err);
+    if (err) return callback(err);
 
+    // eslint-disable-next-line no-shadow
     yauzl.fromBuffer(res.body, (err, zipfile) => {
-      // eslint-disable-next-line keyword-spacing
-      if(err) return callback(err);
+      if (err) return callback(err);
 
       const result = { filenames: [] };
       const entries = [];
       let completed = 0;
 
       zipfile.on('entry', (entry) => entries.push(entry));
-      zipfile.once('end', err => {
+      // eslint-disable-next-line no-shadow
+      zipfile.on('end', (err) => {
+        if (err) return callback(err);
+
         if (entries.length === 0) {
           callback(null, result);
           zipfile.close();
@@ -34,13 +45,11 @@ const zipStreamToFiles = (zipStream, callback) => {
             // eslint-disable-next-line no-shadow
             zipfile.openReadStream(entry, (err, resultStream) => {
 
-              // eslint-disable-next-line keyword-spacing
-              if(err) return callback(err);
+              if (err) return callback(err);
 
               // eslint-disable-next-line no-shadow
               resultStream.pipe(streamTest.toText((err, contents) => {
-                // eslint-disable-next-line keyword-spacing
-                if(err) return callback(err);
+                if (err) return callback(err);
 
                 result[entry.fileName] = contents;
                 completed += 1;
@@ -60,14 +69,3 @@ const zipStreamToFiles = (zipStream, callback) => {
 const pZipStreamToFiles = (zipStream) => new Promise((resolve, reject) => { zipStreamToFiles(zipStream, (err, result) => err ? reject(err) : resolve(result)); });
 
 module.exports = { zipStreamToFiles, pZipStreamToFiles };
-
-function binaryParser(res, callback) {
-  res.setEncoding('binary');
-  res.data = '';
-  res.on('data', function (chunk) {
-    res.data += chunk;
-  });
-  res.on('end', function () {
-    callback(null, new Buffer(res.data, 'binary'));
-  });
-}
