b811300071fc9fee9c7edcbb256a40248a25f020
alxndrsn <alxndrsn>
2024-02-06 06:29:18 +0000

    Revert "remove node-fetch"
    This reverts commit 4b16e4d8ef35f5c183aef9c2bd86b416d80c54d0.

--- test/e2e/soak/index.js
+++ test/e2e/soak/index.js
@@ -8,6 +8,7 @@
 // except according to the terms contained in the LICENSE file.
 
 const fs = require('node:fs');
+const fetch = require('node-fetch');
 const _ = require('lodash');
 const uuid = require('uuid').v4;
 const { basename } = require('node:path');
