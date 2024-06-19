673b500e1e520d21c8210173070a494663a10799
alxndrsn <alxndrsn>
2024-06-19 07:40:57 +0000

    test/e2e: add test for weird instanceId handling

---
+++ test/e2e/standard/test.js
@@ -0,0 +1,60 @@
+// Copyright 2022 ODK Central Developers
+// See the NOTICE file at the top-level directory of this distribution and at
+// https://github.com/getodk/central-backend/blob/master/NOTICE.
+// This file is part of ODK Central. It is subject to the license terms in
+// the LICENSE file found in the top-level directory of this distribution and at
+// https://www.apache.org/licenses/LICENSE-2.0. No part of ODK Central,
+// including this file, may be copied, modified, propagated, or distributed
+// except according to the terms contained in the LICENSE file.
+
+/* eslint-disable */
+
+const { execSync } = require('node:child_process');
+const fs = require('node:fs');
+const { randomBytes } = require('node:crypto');
+const { basename } = require('node:path');
+const _ = require('lodash');
+const { program } = require('commander');
+const should = require('should');
+
+const SUITE_NAME = 'test/e2e/standard';
+const log = require('../util/logger')(SUITE_NAME);
+const { apiClient, mimetypeFor, Redirect } = require('../util/api');
+
+const serverUrl = 'http://localhost:8383';
+const userEmail = 'x@example.com';
+const userPassword = 'secret1234';
+
+const attDir = './test-attachments';
+const BIGFILE = `${attDir}/big.bin`;
+
+describe('standard', () => {
+  let api, projectId, xmlFormId;
+
+  it('should handle weird submission instanceId gracefully', async function() {
+    // given
+    api = await apiClient(SUITE_NAME, { serverUrl, userEmail, userPassword });
+    projectId = await createProject();
+    xmlFormId = await uploadForm('test-form.xml');
+    // TODO upload submission with weird ID
+
+    // when
+    // TODO download formName.svc/Submissions(instanceId)
+
+    // then
+    // assert service has not crashed
+  });
+
+  async function createProject() {
+    const project = await api.apiPostJson(
+      'projects',
+      { name:`standard-test-${new Date().toISOString().replace(/\..*/, '')}` },
+    );
+    return project.id;
+  }
+
+  async function uploadForm(xmlFilePath) {
+    const { xmlFormId } = await api.apiPostFile(`projects/${projectId}/forms`, xmlFilePath);
+    return xmlFormId;
+  }
+});
