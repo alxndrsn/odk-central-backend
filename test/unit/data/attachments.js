c1dd2fcb9a57ef8942905d324b2c1fc9d2856628
alxndrsn <alxndrsn>
2024-06-24 08:59:47 +0000

    lint2

--- test/unit/data/attachments.js
+++ test/unit/data/attachments.js
@@ -5,80 +5,73 @@ const { streamAttachments } = require(appRoot + '/lib/data/attachments');
 const { zipStreamFromParts } = require(appRoot + '/lib/util/zip');
 
 describe('.zip attachments streaming', () => {
-  [false, true].forEach(s3enabled => {
-    // REVIEW s3 enabled/disabled doesn't make any difference to these tests
-    describe(`with s3 blob storage ${s3enabled ? 'enabled' : 'disabled'}`, () => {
-      const mockS3 = { enabled: s3enabled };
+  it('should stream the contents to files at the appropriate paths', (done) => {
+    const inStream = streamTest.fromObjects([
+      { row: { instanceId: 'subone', name: 'firstfile.ext', content: 'this is my first file' } },
+      { row: { instanceId: 'subone', name: 'secondfile.ext', content: 'this is my second file' } },
+      { row: { instanceId: 'subtwo', name: 'thirdfile.ext', content: 'this is my third file' } }
+    ]);
+    zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
+      // eslint-disable-next-line keyword-spacing
+      if(err) return done(err);
 
-      it('should stream the contents to files at the appropriate paths', (done) => {
-        const inStream = streamTest.fromObjects([
-          { row: { instanceId: 'subone', name: 'firstfile.ext', content: 'this is my first file' } },
-          { row: { instanceId: 'subone', name: 'secondfile.ext', content: 'this is my second file' } },
-          { row: { instanceId: 'subtwo', name: 'thirdfile.ext', content: 'this is my third file' } }
-        ]);
-        zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
-          // eslint-disable-next-line keyword-spacing
-          if(err) return done(err);
+      result.filenames.should.eql([
+        'media/firstfile.ext',
+        'media/secondfile.ext',
+        'media/thirdfile.ext'
+      ]);
 
-          result.filenames.should.eql([
-            'media/firstfile.ext',
-            'media/secondfile.ext',
-            'media/thirdfile.ext'
-          ]);
+      result['media/firstfile.ext'].should.equal('this is my first file');
+      result['media/secondfile.ext'].should.equal('this is my second file');
+      result['media/thirdfile.ext'].should.equal('this is my third file');
 
-          result['media/firstfile.ext'].should.equal('this is my first file');
-          result['media/secondfile.ext'].should.equal('this is my second file');
-          result['media/thirdfile.ext'].should.equal('this is my third file');
+      done();
+    });
+  });
 
-          done();
-        });
-      });
+  it('should deal with unsafe filenames sanely', (done) => {
+    const inStream = streamTest.fromObjects([
+      { row: { instanceId: '../subone', name: 'firstfile.ext', content: 'this is my first file' } },
+      { row: { instanceId: 'subone', name: '../secondfile.ext', content: 'this is my second file' } },
+      { row: { instanceId: 'subone', name: './.secondfile.ext', content: 'this is my duplicate second file' } },
+    ]);
+    zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
+      // eslint-disable-next-line keyword-spacing
+      if(err) return done(err);
 
-      it('should deal with unsafe filenames sanely', (done) => {
-        const inStream = streamTest.fromObjects([
-          { row: { instanceId: '../subone', name: 'firstfile.ext', content: 'this is my first file' } },
-          { row: { instanceId: 'subone', name: '../secondfile.ext', content: 'this is my second file' } },
-          { row: { instanceId: 'subone', name: './.secondfile.ext', content: 'this is my duplicate second file' } },
-        ]);
-        zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
-          // eslint-disable-next-line keyword-spacing
-          if(err) return done(err);
+      result.filenames.should.eql([
+        'media/firstfile.ext',
+        'media/..secondfile.ext',
+        'media/..secondfile.ext'
+      ]);
 
-          result.filenames.should.eql([
-            'media/firstfile.ext',
-            'media/..secondfile.ext',
-            'media/..secondfile.ext'
-          ]);
+      done();
+    });
+  });
 
-          done();
-        });
-      });
+  it('should not strip .enc unless decryption is happening', (done) => {
+    const inStream = streamTest.fromObjects([
+      { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
+    ]);
+    zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
+      // eslint-disable-next-line keyword-spacing
+      if(err) return done(err);
 
-      it('should not strip .enc unless decryption is happening', (done) => {
-        const inStream = streamTest.fromObjects([
-          { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
-        ]);
-        zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
-          // eslint-disable-next-line keyword-spacing
-          if(err) return done(err);
+      result.filenames.should.eql([ 'media/firstfile.ext.enc' ]);
+      done();
+    });
+  });
 
-          result.filenames.should.eql([ 'media/firstfile.ext.enc' ]);
-          done();
-        });
-      });
+  it('should strip .enc if decryption is happening', (done) => {
+    const inStream = streamTest.fromObjects([
+      { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
+    ]);
+    zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream, () => {})), (err, result) => {
+      // eslint-disable-next-line keyword-spacing
+      if(err) return done(err);
 
-      it('should strip .enc if decryption is happening', (done) => {
-        const inStream = streamTest.fromObjects([
-          { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
-        ]);
-        zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream, () => {})), (err, result) => {
-          // eslint-disable-next-line keyword-spacing
-          if(err) return done(err);
-
-          result.filenames.should.eql([ 'media/firstfile.ext' ]);
-          done();
-        });
-      });
+      result.filenames.should.eql([ 'media/firstfile.ext' ]);
+      done();
     });
   });
 });
