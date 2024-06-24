0d957d7c2a70761157ebc4ec45577acbe9993945
alxndrsn <alxndrsn>
2024-06-24 09:07:36 +0000

    fix unit tests

--- test/unit/data/attachments.js
+++ test/unit/data/attachments.js
@@ -1,15 +1,18 @@
 const appRoot = require('app-root-path');
 const streamTest = require('streamtest').v2;
 const { zipStreamToFiles } = require(appRoot + '/test/util/zip');
+const { PartialPipe } = require(appRoot + '/lib/util/stream');
 const { streamAttachments } = require(appRoot + '/lib/data/attachments');
 const { zipStreamFromParts } = require(appRoot + '/lib/util/zip');
 
 describe('.zip attachments streaming', () => {
   it('should stream the contents to files at the appropriate paths', (done) => {
-    const inStream = streamTest.fromObjects([
-      { row: { instanceId: 'subone', name: 'firstfile.ext', content: 'this is my first file' } },
-      { row: { instanceId: 'subone', name: 'secondfile.ext', content: 'this is my second file' } },
-      { row: { instanceId: 'subtwo', name: 'thirdfile.ext', content: 'this is my third file' } }
+    const inStream = new PartialPipe([
+      streamTest.fromObjects([
+        { row: { instanceId: 'subone', name: 'firstfile.ext', content: 'this is my first file' } },
+        { row: { instanceId: 'subone', name: 'secondfile.ext', content: 'this is my second file' } },
+        { row: { instanceId: 'subtwo', name: 'thirdfile.ext', content: 'this is my third file' } }
+      ]),
     ]);
     zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
       // eslint-disable-next-line keyword-spacing
@@ -30,10 +33,12 @@ describe('.zip attachments streaming', () => {
   });
 
   it('should deal with unsafe filenames sanely', (done) => {
-    const inStream = streamTest.fromObjects([
-      { row: { instanceId: '../subone', name: 'firstfile.ext', content: 'this is my first file' } },
-      { row: { instanceId: 'subone', name: '../secondfile.ext', content: 'this is my second file' } },
-      { row: { instanceId: 'subone', name: './.secondfile.ext', content: 'this is my duplicate second file' } },
+    const inStream = new PartialPipe([
+      streamTest.fromObjects([
+        { row: { instanceId: '../subone', name: 'firstfile.ext', content: 'this is my first file' } },
+        { row: { instanceId: 'subone', name: '../secondfile.ext', content: 'this is my second file' } },
+        { row: { instanceId: 'subone', name: './.secondfile.ext', content: 'this is my duplicate second file' } },
+      ]),
     ]);
     zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
       // eslint-disable-next-line keyword-spacing
@@ -50,8 +55,10 @@ describe('.zip attachments streaming', () => {
   });
 
   it('should not strip .enc unless decryption is happening', (done) => {
-    const inStream = streamTest.fromObjects([
-      { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
+    const inStream = new PartialPipe([
+      streamTest.fromObjects([
+        { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
+      ]),
     ]);
     zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream)), (err, result) => {
       // eslint-disable-next-line keyword-spacing
@@ -63,8 +70,10 @@ describe('.zip attachments streaming', () => {
   });
 
   it('should strip .enc if decryption is happening', (done) => {
-    const inStream = streamTest.fromObjects([
-      { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
+    const inStream = new PartialPipe([
+      streamTest.fromObjects([
+        { row: { instanceId: 'subone', name: 'firstfile.ext.enc', content: 'this is my first file' } }
+      ]),
     ]);
     zipStreamToFiles(zipStreamFromParts(streamAttachments(inStream, () => {})), (err, result) => {
       // eslint-disable-next-line keyword-spacing
