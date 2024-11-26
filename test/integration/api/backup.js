58a2de2d3872920c5ac45d87273bf10029e57539
alxndrsn <alxndrsn>
2024-11-26 08:48:56 +0000

    wip

--- test/integration/api/backup.js
+++ test/integration/api/backup.js
@@ -1,11 +1,18 @@
 const { testService } = require('../setup');
+const { httpZipResponseToFiles } = require('../../util/zip');
 
-
-describe('api: /backup', () => {
-  describe('POST', () => {
+describe.only('api: /backup', () => {
+  describe('POST', function () {
+    this.timeout(10000);
     it('should reject if the user cannot backup', testService((service) =>
       service.login('chelsea', (asChelsea) =>
         asChelsea.post('/v1/backup').expect(403))));
+
+    it('should return TODO if the user can backup @slow', testService((service) => {
+      return service.login('alice', (asAlice) =>
+        httpZipResponseToFiles(asAlice.post('/v1/backup'))
+          .then(res => console.log('res:', res)));
+    }));
   });
 });
 
