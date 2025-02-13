28c047d6b0fe19456d9cfc31525cf36b38bc5de0
alxndrsn <alxndrsn>
2025-02-13 10:54:04 +0000

    wip

--- test/integration/api/audits.js
+++ test/integration/api/audits.js
@@ -796,7 +796,8 @@ describe('/audits', () => {
         service.login('alice', (asAlice) =>
           asAlice.post('/v1/projects/1/forms/simple/public-links')
             .send({ displayName: 'Public Link Name' })
-            .then(({ body }) => body)
+            .expect(200)
+            .then(({ body }) => console.log('body:', body) || body)
             .then((link) => asAlice.delete('/v1/projects/1/forms/simple/public-links/' + link.id))
             .then(() => asAlice.get('/v1/audits').set('X-Extended-Metadata', true))
             .then(({ body }) => {
