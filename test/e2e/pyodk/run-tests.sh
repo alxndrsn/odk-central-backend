3f15b2bc8983b43c7b27cb8240ee86f79bc2b903
alxndrsn <alxndrsn>
2025-04-30 07:44:18 +0000

    Add test runner

---
+++ test/e2e/pyodk/run-tests.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -eu
+set -o pipefail
+
+serverUrl="http://localhost:8383"
+userEmail="x@example.com"
+userPassword="secret1234"
+
+log() { echo "[test/e2e/standard/run-tests] $*"; }
+
+log "Building backend..."
+make base
+
+log "Attempting to create user..."
+echo "$userPassword" | node ./lib/bin/cli.js user-create  -u "$userEmail" && log "User created."
+log "Attempting to promote user..."
+node ./lib/bin/cli.js user-promote -u "$userEmail" && log "User promoted."
+
+kill_child_processes() {
+  log "Killing child processes..."
+  kill -- -$$ || true
+}
+trap kill_child_processes EXIT
+
+log "Starting backend..."
+make run | tee server.log &
+
+log "Waiting for backend to start..."
+timeout 30 bash -c "while ! curl -s -o /dev/null $serverUrl; do sleep 1; done"
+log "Backend started!"
+
+log "Building pyodk test image..."
+imgTag="pyodk-central-tester"
+docker build --tag "$imgTag" .
+log "Image built OK."
+
+log "Running tests..."
+docker run "$imgTag"
+
+if ! curl -s -o /dev/null "$serverUrl"; then
+  log "Backend died."
+  exit 1
+fi
+
+log "Tests completed OK."
