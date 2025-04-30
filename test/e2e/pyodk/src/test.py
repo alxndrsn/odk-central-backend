161403f662de2c2e07fdb65dc45caf7f2c4bec4e
alxndrsn <alxndrsn>
2025-04-30 07:55:44 +0000

    create project & form before uploading submission

--- test/e2e/pyodk/src/test.py
+++ test/e2e/pyodk/src/test.py
@@ -9,17 +9,51 @@ log("Welcome")
 
 log("Initialising pyodk client...")
 with Client(config_path="pyodk.conf.toml") as client:
-  log("TODO Attempting to create project...")
+  log('Creating project...')
+  res = client.post("projects", data='{"name":"test project 1"}', headers={'content-type': 'application/json'})
+  assert res.status_code == 200, f'Failed to create project.  HTTP response code: {res.status_code}'
 
-  log("TODO Attempting to upload form...")
+  log("Uploading form...")
+  client.forms.create(
+    definition = (
+      f'<?xml version="1.0"?>'
+      f'<h:html xmlns="http://www.w3.org/2002/xforms" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms" xmlns:odk="http://www.opendatakit.org/xforms">'
+      f'  <h:head>'
+      f'    <h:title>upload_file</h:title>'
+      f'    <model odk:xforms-version="1.0.0">'
+      f'      <instance>'
+      f'        <data id="upload_file" version="1">'
+      f'          <name/>'
+      f'          <file/>'
+      f'          <meta>'
+      f'            <instanceID/>'
+      f'          </meta>'
+      f'        </data>'
+      f'      </instance>'
+      f'      <bind nodeset="/data/name" type="string"/>'
+      f'      <bind nodeset="/data/file" type="binary"/>'
+      f'      <bind nodeset="/data/meta/instanceID" type="string" readonly="true()" jr:preload="uid"/>'
+      f'    </model>'
+      f'  </h:head>'
+      f'  <h:body>'
+      f'    <input ref="/data/name">'
+      f'      <label>Name</label>'
+      f'    </input>'
+      f'    <upload mediatype="application/*" ref="/data/file">'
+      f'      <label>File</label>'
+      f'    </upload>'
+      f'  </h:body>'
+      f'</h:html>'
+    ),
+  )
+  # TODO check success?
 
-  log("Attempting to upload submission...")
   file = Path(__file__).parent / "fruits.csv"
   submissionId = f'uuid:{uuid.uuid4()}'
 
-  log("Uploading submission with:")
-  log("  submission ID:", submissionId)
-  log("  attachment:", file.name)
+  log('Uploading submission with:')
+  log('  submission ID:', submissionId)
+  log('  attachment:', file.name)
 
   submission = client.submissions.create(
     xml = (
