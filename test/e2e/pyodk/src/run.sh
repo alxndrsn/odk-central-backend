08d9ddbae5029e783553288c304e9046337556c2
alxndrsn <alxndrsn>
2025-04-30 08:10:58 +0000

    test session creation

--- test/e2e/pyodk/src/run.sh
+++ test/e2e/pyodk/src/run.sh
@@ -6,6 +6,11 @@ log() { echo "[$(basename "$0")] $*"; }
 log "Checking connection..."
 curl -v http://localhost:8383/v1/
 
+log "Testing session creation..."
+curl http://localhost:8383/v1/sessions \
+    -H 'content-type: application/json' \
+    --data '{"email":"x@example.com", "password":"secret1234"}'
+
 log "pyodk.conf.toml:"
 cat pyodk.conf.toml
 
