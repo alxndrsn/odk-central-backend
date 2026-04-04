dc546344dd22024cb2df211f93db6be2303b4a48
alxndrsn <alxndrsn>
2026-04-04 06:43:14 +0000

    no loge

--- test/e2e/soak/ci/prepare-postgres.sh
+++ test/e2e/soak/ci/prepare-postgres.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -eu
 set -o pipefail
 
-logPrefix="[test/e2e/soak/$(basename "$0" .sh)]"
-log()  { echo    >&2 "$logPrefix $*"; }
-loge() { echo -e >&2 "$logPrefix $*"; }
+log()  { echo >&2 "[test/e2e/soak/$(basename  $*"; }
 
 pg_exec() {
   PGPASSWORD=odktest \
@@ -25,7 +23,7 @@ pg_exec "ALTER SYSTEM SET track_activity_query_size = 16384"
 
 # Postgres must be restarted to apply change to track_activity_query_size
 # See: https://www.postgresql.org/docs/14/runtime-config-statistics.html#GUC-TRACK-ACTIVITY-QUERY-SIZE
-loge "  Restarting postgres...\n$logPrefix     "
+log "  Restarting postgres..."
 pgImg="$(docker ps -q --filter name=postgres)"
 docker restart "$pgImg" >/dev/null
 timeout 10 bash -c "while ! docker exec $pgImg pg_isready --timeout=1; do sleep 1; done"
