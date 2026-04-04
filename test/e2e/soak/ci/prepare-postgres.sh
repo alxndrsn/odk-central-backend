2773227d67c1e2fc8f610e977de05de3d3fde2b3
alxndrsn <alxndrsn>
2026-04-04 05:55:10 +0000

    check final size; improve logging

--- test/e2e/soak/ci/prepare-postgres.sh
+++ test/e2e/soak/ci/prepare-postgres.sh
@@ -20,16 +20,31 @@ pg_exec() {
 
 log "Increasing query log lengths..."
 
-log "  track_activity_query_size: $(pg_exec 'SHOW track_activity_query_size')"
+log "track_activity_query_size: $(pg_exec 'SHOW track_activity_query_size')"
 pg_exec "ALTER SYSTEM SET track_activity_query_size = 16384"
 pgImg="$(docker ps -q --filter name=postgres)"
 
-log "  restarting service..."
+# Postgres must be restarted to apply change to track_activity_query_size
+# See: https://www.postgresql.org/docs/14/runtime-config-statistics.html#GUC-TRACK-ACTIVITY-QUERY-SIZE
+log "Restarting postgres..."
 docker restart "$pgImg"
 timeout 10 bash -c "while ! docker exec $pgImg pg_isready --timeout=1; do sleep 1; done"
-log "  restarted OK."
+log "Restarted OK."
 
-log "  track_activity_query_size: $(pg_exec 'SHOW track_activity_query_size')"
-log "  DONE."
+finalQuerySize="$(pg_exec 'SHOW track_activity_query_size')"
+log "track_activity_query_size: $finalQuerySize"
+
+expectedFinalQuerySize=16kb
+if [[ $finalQuerySize != $expectedFinalQuerySize ]]; then
+  log "!!!"
+  log "!!! Failed to set final query size."
+  log "!!!"
+  log "!!!   expected: $expectedFinalQuerySize"
+  log "!!!     actual: $finalQuerySize"
+  log "!!!"
+  exit 1
+fi
+
+log "DONE."
 
 exit 1
